6edd686f891312994e3f71b6f06197b6d8da23f8
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2024-10-18 14:35:00 +0200

    restaurant api frontend

---
+++ OrderingApp.Restaurant.Api/Controllers/RestaurantController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderingApp.Restaurant.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RestaurantController : ControllerBase
+    {
+        public RestaurantController()
+        {
+        }
+    }
+}
